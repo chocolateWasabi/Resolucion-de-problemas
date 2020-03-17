f541f26d676b6cc969fb2d50839c6d7ebb72f17f
y0s30b <coacervate@g.skku.edu>
2020-03-17 21:29:05 +0900

    a%n>=b%n && n>0 일 때, a%n-b%n=(a-b)%n 의 반례 찾기

--- self.__exercise__/noname.cpp
+++ self.__exercise__/noname.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
-#define 2 4
-#define 4 2
+using namespace std;
 
+#define max 5
 int main(){
-    // 아 안 되네..
+
+    int a, b, n;
+    int count = 0;
+    for(a=-max;a<=max;++a){
+        for(b=-max;b<=max;++b){
+            for(n=1;n<=max;++n){/*
+                if(a % n - b % n == (a - b) % n){
+                   // cout << a % n << ' ' << b % n << ' ' << (a - b) % n << endl;
+                    count ++;
+                }
+                else {
+                    cout << a << ' ' << b << ' ' << a % n << ' ' << b % n << ' ' << n << endl;
+                }*/
+
+                if(a % n >= b % n){
+                    if(a % n - b % n != (a - b) % n){
+                        cout << a << ' ' << b << ' ' << n << " | " << a % n << ' ' << b % n << ' ' << (a - b) % n << endl;
+                    }
+                }
+            }
+        }
+    }
+    cout << count;
+    return 0;
 }
