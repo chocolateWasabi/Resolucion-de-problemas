c775551faaeb534ebb7e96e3e697aab8386c1113
y0s30b <coacervate@g.skku.edu>
2020-03-21 17:40:59 +0900

    SWEA 1248 AC (class pointer로 binary tree 구현)

--- SWEA/1248.cpp
+++ SWEA/1248.cpp
@@ -1,92 +1,221 @@
 // SWEA 1248 공통조상 문제 (D5)
 #include <iostream>
-#include <cstring>
+//#include <fstream>
 #include <vector>
 using namespace std;
 
-int vertex[10010]; // tree에서의 정점의 위치
-int pre_tree[10010][2]; // 서브 트리가 먼저 입력되는 경우를 대비한 전처리
-int tree[100000]; 
-// 크기가 최대 2^10000 (skewed tree 인 경우...)
-// 결국 배열로 해결 불가.
-
-void make_tree(int node, int pos){
-    tree[pos] = node;
-    vertex[node] = pos;
-
-    if(pre_tree[node][0]){
-        make_tree(pre_tree[node][0], 2 * pos);
+class tree_node {
+private:
+    tree_node *parent, *l_child, *r_child;
+    int value;
+public:
+    tree_node* get_parent(){
+        return parent;
     }
-    if(pre_tree[node][1]){
-        make_tree(pre_tree[node][1], 2 * pos + 1);
+    tree_node* get_l_child(){
+        return l_child;
+    }
+    tree_node* get_r_child(){
+        return r_child;
+    }
+    tree_node(int value){
+        this->value = value;
+    }
+    void set_parent(tree_node* parent){
+        this->parent = parent;
+    }
+    void set_l_child(tree_node* l_child){
+        this->l_child = l_child;
+    }
+    void set_r_child(tree_node* r_child){
+        this->r_child = r_child;
+    }
+    int get_value(){
+        return value;
+    }
+};
+//ifstream fin;
+
+class common_ancestor {
+private:
+    tree_node* vertex[10010]; // 각 정점의 메모리 위치 정보
+    tree_node* root;
+    int v, e, t1, t2;
+    int subtree_size;
+    int trial;
+    void make_tree(int parent, int child);
+    void traverse_tree(tree_node* node);
+    void get_subtree_size(tree_node* node);
+    void ca_main();
+public:
+    common_ancestor(int trial){
+        this->trial = trial;    
+        root = new tree_node(1);
+        root->set_parent(root);
+        root->set_l_child(nullptr);
+        root->set_r_child(nullptr);
+        vertex[1] = root;
+        subtree_size = 0;
+        ca_main();
+    }
+    ~common_ancestor(){ // 트리 정보 모두 삭제
+        for(int i=1;i<=v;++i){
+            delete vertex[i];
+        }
+    }
+};
+
+void common_ancestor::make_tree(int parent, int child){
+    if(vertex[parent] == nullptr){
+        tree_node* tmp = new tree_node(parent);
+        tmp->set_parent(nullptr);
+
+        if(vertex[child] == nullptr){ // P(X) C(X)
+            tree_node* tmp2 = new tree_node(child);
+            tmp2->set_parent(tmp);
+            tmp2->set_l_child(nullptr);
+            tmp2->set_r_child(nullptr);
+
+            tmp->set_l_child(tmp2);
+            vertex[child] = tmp2;
+        }
+        else { // P(X) C(O)
+            tmp->set_l_child(vertex[child]);
+            vertex[child]->set_parent(tmp);
+        }
+
+        // 공통 작업
+        tmp->set_r_child(nullptr);
+        vertex[parent] = tmp;
+    }
+    else {
+        if(vertex[child] == nullptr){ // P(O) C(X)
+            tree_node* tmp2 = new tree_node(child);
+            tmp2->set_parent(vertex[parent]);
+            tmp2->set_l_child(nullptr);
+            tmp2->set_r_child(nullptr);
+            vertex[child] = tmp2;
+
+            // 부모와 연결
+            if(vertex[parent]->get_l_child() == nullptr){
+                vertex[parent]->set_l_child(tmp2);
+            }
+            else {
+                vertex[parent]->set_r_child(tmp2);
+            }
+        }
+        else { // P(O) C(O) : 둘 다 존재하는데 아직 연결이 안 된 경우, C subtree를 P에 연결하는 작업.
+            if(vertex[parent]->get_l_child() == nullptr){
+                vertex[parent]->set_l_child(vertex[child]);
+            }
+            else {
+                vertex[parent]->set_r_child(vertex[child]);
+            }
+            vertex[child]->set_parent(vertex[parent]);
+        }
     }
 }
 
-void get_size(int node_pos, int& size){
-    if(tree[2*node_pos]){
-        size ++;        
-        get_size(2*node_pos, size);
+void common_ancestor::traverse_tree(tree_node* node){
+    // 메모리 접근에 주의해서 출력
+    cout << "node:" << node->get_value();
+    if(node->get_l_child() != nullptr){
+        cout << " l_child:" << node->get_l_child()->get_value();
     }
-    if(tree[2*node_pos+1]){
-        size ++;
-        get_size(2*node_pos+1, size);
+    else {
+        cout << " l_child:nullptr";
     }
+    if(node->get_r_child() != nullptr){
+        cout << " r_child:" << node->get_r_child()->get_value();
+    }
+    else {
+        cout << " r_child:nullptr" ;
+    }
+    if(node->get_parent() != nullptr){
+        cout << " parent:" << node->get_parent()->get_value();
+    }
+    else {
+        cout << " parent:nullptr";
+    }
+    cout << '\n';
+
+    // 순회
+    if(node->get_l_child() != nullptr){
+        traverse_tree(node->get_l_child());
+    }
+    if(node->get_r_child() != nullptr){
+        traverse_tree(node->get_r_child());
+    }
+}
+
+void common_ancestor::get_subtree_size(tree_node* node){
+    subtree_size ++;
+    if(node->get_l_child() != nullptr){
+        get_subtree_size(node->get_l_child());
+    }
+    if(node->get_r_child() != nullptr){
+        get_subtree_size(node->get_r_child());
+    }
+}
+
+void common_ancestor::ca_main(){
+    cin >> v >> e >> t1 >> t2;
+    //fin >> v >> e >> t1 >> t2;
+    for(int i=2;i<=v;++i){
+        vertex[i] = nullptr;
+    }
+    for(int i=1, parent, child;i<=e;++i){
+
+        cin >> parent >> child;
+        //fin >> parent >> child;
+        make_tree(parent, child);
+    }
+    // traverse_tree(root);
+    vector<int> ancestors;
+    for(auto i=vertex[t1]->get_parent();i->get_value() != 1;i=i->get_parent()){
+        ancestors.push_back(i->get_value());
+    }
+    // ancestors.push_back(1);
+    bool find_flag = false;
+    int where_common = 1;
+    for(auto i=vertex[t2]->get_parent();i->get_value() != 1 && !find_flag;i=i->get_parent()){
+        for(auto j:ancestors){
+            if(j == i->get_value()){
+                find_flag = true;
+                where_common = j;
+                break;
+            }
+        }
+    }
+
+    // get size of subtree
+    get_subtree_size(vertex[where_common]);
+    cout << '#' << trial << ' ' << where_common << ' ' << subtree_size << '\n';
 }
 
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
+    //ifstream fin;
+    //fin.open("C:/inputt.txt");
     int T;
     cin >> T;
-    int v, e, t1, t2;
-    vector<int> ancestor;
-    bool find_flag;
-    int where_common, size;
-    for(int t=1;t<=T;++t){
-        cin >> v >> e >> t1 >> t2;
-        
-        // 초기화
-        memset(vertex, 0, sizeof(vertex));
-        memset(pre_tree, 0, sizeof(pre_tree));
-        memset(tree, 0, sizeof(tree));
-        ancestor.clear();
-        
-        // 간선 정보 build up (무조건 루트는 1)
-        for(int i=1, parent, child;i<=e;++i){
-            cin >> parent >> child;
-            if(!pre_tree[parent][0]){
-                pre_tree[parent][0] = child;
-            }
-            else {
-                pre_tree[parent][1] = child;
-            }
+    //if(fin.is_open()){
+        //fin >> T;
+        for(int t=1;t<=T;++t){
+            common_ancestor *get_common = new common_ancestor(t);
+            delete get_common;
         }
+        //fin.close();
 
-        // 트리 생성
-        make_tree(1, 1);
-        
-        // 검사: tree에서 t1, t2의 위치 = vertex[t1], vertex[t2]
-        find_flag = false;
-        where_common = 1;
-        for(int i=vertex[t1]/2;i>=1;i/=2){
-            ancestor.push_back(tree[i]);
-        }
-        for(int i=vertex[t2]/2;i>=1 && !find_flag;i/=2){
-            for(auto j:ancestor){
-                if(j == tree[i]){
-                    find_flag = true;
-                    where_common = tree[i];
-                    break;
-                }
-            }
-        }
+        // cout << "opened";
 
-        // 서브 트리의 크기 구하기
-        size = 1;
-        get_size(vertex[where_common], size);
-
-        cout << '#' << t << ' ' << where_common << ' ' << size << '\n';
-    }
+        // delete[] 는 배열 없앨 때
+        // delete 는 그냥 원소 하나 없앨 때. delete 써야할 때 delete[] 쓰면 런타임 에러 발생.
+    /*}
+    else {
+        cout << "file error\n";
+    }*/
     return 0;
 }
