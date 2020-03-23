6e3265aac15278cfbe953e6acc822b4dc1734e41
y0s30b <coacervate@g.skku.edu>
2020-03-23 23:59:33 +0900

    SWEA 4013 ...ing

---
+++ SWEA/4013.cpp
@@ -0,0 +1,163 @@
+// SWEA 4013 특이한 자석 - 시뮬레이션
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+void rotate_mag(int *magnetic, int cw){
+    if(cw == 1){
+        int tmp = magnetic[7];
+        for(int i=0;i<7;++i){
+            magnetic[i+1] = magnetic[i];
+        }
+        magnetic[0] = tmp;
+    }
+    else { // cw == -1
+        int tmp = magnetic[0];
+        for(int i=0;i<7;++i){
+            magnetic[i] = magnetic[i+1];
+        }
+        magnetic[7] = tmp;
+    }
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int T, K;
+    int magnetic[4][8], mag_n, cw;
+    cin >> T;
+    for(int t=1;t<=T;++t){
+        cin >> K;
+        for(int i=0;i<4;++i){
+            for(int j=0;j<8;++j){
+                cin >> magnetic[i][j];
+            }
+        }
+        for(int i=0;i<K;++i){
+            cin >> mag_n >> cw;
+            switch(mag_n){
+            case 1:
+                if(magnetic[0][2] != magnetic[1][6]){ // 1, 2번 자석
+                    if(magnetic[1][2] != magnetic[2][6]){ // 1, 2, 3번 자석
+                        if(magnetic[2][2] != magnetic[3][6]){ // 1, 2, 3, 4번 자석
+                            rotate_mag(magnetic[0], cw);
+                            rotate_mag(magnetic[1], -cw);
+                            rotate_mag(magnetic[2], cw);
+                            rotate_mag(magnetic[3], -cw);
+                        }
+                        else{
+                            rotate_mag(magnetic[0], cw);
+                            rotate_mag(magnetic[1], -cw);
+                            rotate_mag(magnetic[2], cw);
+                        }
+                    }
+                    else{
+                        rotate_mag(magnetic[0], cw);
+                        rotate_mag(magnetic[1], -cw);
+                    }
+                }
+                else{ // 1번 자석
+                    rotate_mag(magnetic[0], cw);
+                }
+            break;
+            case 2:
+                if(magnetic[1][6] == magnetic[0][2]){
+                    if(magnetic[1][2] == magnetic[2][6]){
+                        rotate_mag(magnetic[1], cw);
+                    }
+                    else{
+                        if(magnetic[2][2] == magnetic[3][6]){
+                            rotate_mag(magnetic[1], cw);
+                            rotate_mag(magnetic[2], -cw);
+                        }
+                        else{
+                            rotate_mag(magnetic[1], cw);
+                            rotate_mag(magnetic[2], -cw);
+                            rotate_mag(magnetic[3], cw);
+                        }
+                    }
+                }
+                else{
+                    if(magnetic[1][2] == magnetic[2][6]){
+                        rotate_mag(magnetic[0], -cw);
+                        rotate_mag(magnetic[1], cw);
+                    }
+                    else{
+                        if(magnetic[2][2] == magnetic[3][6]){
+                            rotate_mag(magnetic[0], -cw);
+                            rotate_mag(magnetic[1], cw);
+                            rotate_mag(magnetic[2], -cw);
+                        }
+                        else{
+                            rotate_mag(magnetic[0], -cw);
+                            rotate_mag(magnetic[1], cw);
+                            rotate_mag(magnetic[2], -cw);
+                            rotate_mag(magnetic[3], cw);
+                        }
+                    }
+                }
+            break;
+            case 3:
+                if(magnetic[3][6] == magnetic[2][2]){
+                    if(magnetic[1][2] == magnetic[2][6]){
+                        rotate_mag(magnetic[2], cw);
+                    }
+                    else{
+                        if(magnetic[2][2] == magnetic[3][6]){
+                            rotate_mag(magnetic[2], cw);
+                        }
+                        else{
+                            rotate_mag(magnetic[2], cw);
+                        }
+                    }
+                }
+                else{
+                    if(magnetic[1][2] == magnetic[2][6]){
+                        rotate_mag(magnetic[2], cw);
+                        rotate_mag(magnetic[3], -cw);
+                    }
+                    else{
+                        if(magnetic[2][2] == magnetic[3][6]){
+                            rotate_mag(magnetic[2], cw);
+                            
+                            rotate_mag(magnetic[3], -cw);
+                        }
+                        else{
+                            rotate_mag(magnetic[2], cw);
+                            rotate_mag(magnetic[3], -cw);
+                        }
+                    }
+                }
+            break;
+            case 4:
+                if(magnetic[3][6] != magnetic[2][2]){
+                    if(magnetic[2][6] != magnetic[1][2]){
+                        if(magnetic[1][6] != magnetic[0][2]){
+                            rotate_mag(magnetic[3], cw);
+                            rotate_mag(magnetic[2], -cw);
+                            rotate_mag(magnetic[1], cw);
+                            rotate_mag(magnetic[0], -cw);
+                        }
+                        else{
+                            rotate_mag(magnetic[3], cw);
+                            rotate_mag(magnetic[2], -cw);
+                            rotate_mag(magnetic[1], cw);
+                        }
+                    }
+                    else{
+                        rotate_mag(magnetic[3], cw);
+                        rotate_mag(magnetic[2], -cw);
+                    }
+                }
+                else{
+                    rotate_mag(magnetic[3], cw);
+                }
+            break;
+            }
+        }
+        cout << '#' << t << ' ' << magnetic[0][0]*1 + magnetic[1][0]*2 + magnetic[2][0]*4 + magnetic[3][0]*8 << '\n';
+    }
+
+    return 0;
+}
