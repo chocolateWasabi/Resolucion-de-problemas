8e16cf579108424926e348c046baca50ad7bea91
y0s30b <coacervate@g.skku.edu>
2020-03-26 01:58:53 +0900

    commit for 03.25 - delayed commit, answer for mcsl tryout

--- self.__exercise__/noname.cpp
+++ self.__exercise__/noname.cpp
@@ -1,30 +1,6 @@
-#include <iostream>
-using namespace std;
-
-#define max 5
+#include <cstdio> // skku mcsl lab intern tryout
 int main(){
-
-    int a, b, n;
-    int count = 0;
-    for(a=-max;a<=max;++a){
-        for(b=-max;b<=max;++b){
-            for(n=1;n<=max;++n){/*
-                if(a % n - b % n == (a - b) % n){
-                   // cout << a % n << ' ' << b % n << ' ' << (a - b) % n << endl;
-                    count ++;
-                }
-                else {
-                    cout << a << ' ' << b << ' ' << a % n << ' ' << b % n << ' ' << n << endl;
-                }*/
-
-                if(a % n >= b % n){
-                    if(a % n - b % n != (a - b) % n){
-                        cout << a << ' ' << b << ' ' << n << " | " << a % n << ' ' << b % n << ' ' << (a - b) % n << endl;
-                    }
-                }
-            }
-        }
-    }
-    cout << count;
+    auto func = [](int n){return (n & -n) == n ? 1 : 0;};
+    for(int n;~scanf("%d", &n);printf("%d\n",func(n)?1:0));
     return 0;
 }
