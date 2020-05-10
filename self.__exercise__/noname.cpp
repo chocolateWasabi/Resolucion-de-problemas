8f3b74fa020fb1920083ebd805708eeadb9ca63d
y0s30b <y0s30b.k@gmail.com>
2020-05-10 11:23:53 +0900

    git log-in info test

--- self.__exercise__/noname.cpp
+++ self.__exercise__/noname.cpp
@@ -1,21 +1,7 @@
-#include <cstdio>
-#include <cstdlib>
+#include <iostream>
+using namespace std;
 
 int main(){
-
-    int *a = (int*) malloc(sizeof(int) * 1);
-    int *b = (int*) malloc(sizeof(int) * 1);
-
-    *a = 20;
-    *b = 30;
-    
-    printf("%d %d\n", *a, *b);
-
-    *a ^= *b;
-    *b ^= *a;
-    *a ^= *b;
-
-    printf("%d %d\n", *a, *b);
-    free(a); free(b);
+    cout << "git test";
     return 0;
 }
