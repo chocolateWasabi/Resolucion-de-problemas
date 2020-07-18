87c687008dd69732f6bc13da3903ca45165020d1
y0s30b <y0s30b.k@gmail.com>
2020-07-18 16:29:56 +0900

    mac에서 c++ 빌드 테스트 완료

--- self.__exercise__/noname.cpp
+++ self.__exercise__/noname.cpp
@@ -2,6 +2,14 @@
 using namespace std;
 
 int main(){
-    cout << "git test";
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int T;
+    cin >> T;
+    for(int t=1;t<=T;++t){
+        cout << '#' << t << ' ' << t * 3 << '\n';
+    }
+    
     return 0;
 }
