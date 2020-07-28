291f2bfcd443ab80f0d6fb945557c3994e53fe4c
chocolateWasabi <y0s30b.k@gmail.com>
2020-07-28 20:27:38 +0900

    BOJ ID 12100 부분 정답... 합쳐진 블록 다시 못 합치게 수정 필요

--- Baekjoon/12100.cpp
+++ Baekjoon/12100.cpp
@@ -7,7 +7,8 @@ using namespace std;
 typedef char byte;
 typedef class {
 public:
-    byte board[20][20];
+    byte board[25][25];
+    int convergeCount;
 } gameBoard;
 
 int n;
@@ -15,37 +16,131 @@ bool findBlock[21];
 gameBoard initBoard;
 queue<gameBoard> twoZeroFourEight;
 
+bool isIdentical(gameBoard& a, gameBoard& b) {
+    /*
+    bool full, updown, leftright;
+    full = updown = leftright = true;
+    for(int i=0;i<n;++i) {
+        for(int j=0;j<n;++j) {
+            // 완전히 같은 경우에 대한 체크
+            if(a.board[i][j] != b.board[i][j]) {
+                full = false;
+            }
+
+            // 상하 대칭인 경우에 대한 체크
+            if(a.board[i][j] != b.board[n-i-1][j]) {
+                updown = false;
+            }
+
+            // 좌우 대칭인 경우에 대한 체크
+            if(a.board[i][j] != b.board[i][n-j-1]) {
+                leftright = false;
+            }
+        }
+    }
+    
+    return (full || updown || leftright);*/
+    return false;
+}
+
 void play2048() {
     // BFS pruning
     gameBoard nowBoard, tempBoard;
     twoZeroFourEight.push(initBoard);
     while(!twoZeroFourEight.empty()) {
+        int size = twoZeroFourEight.size();
         nowBoard = twoZeroFourEight.front();
         twoZeroFourEight.pop();
 
+        if(nowBoard.convergeCount >= 5) {
+            continue;
+        }
+
+        tempBoard.convergeCount = nowBoard.convergeCount + 1;
+
         // 우측으로 
-        memcpy(tempBoard.board, nowBoard.board, sizeof(gameBoard));
+        memcpy(tempBoard.board, nowBoard.board, sizeof(nowBoard.board));
         for(int i=0;i<n;++i) {
             for(int j=n-1;j>=1;--j) {
-                if(tempBoard.board[i][j] == tempBoard.board[i][j-1]) {
+                if(tempBoard.board[i][j] != 0 && tempBoard.board[i][j] == tempBoard.board[i][j-1]) {
                     tempBoard.board[i][j] ++;
                     tempBoard.board[i][j-1] = 0;
+                    findBlock[tempBoard.board[i][j]] = true;
+                }
+                if(tempBoard.board[i][j] == 0) { // 0이면 당겨오기
+                    for(int k=j-1;k>=0;--k) {
+                        tempBoard.board[i][k + 1] = tempBoard.board[i][k];
+                    }
+                    tempBoard.board[i][0] = 0;
                 }
             }
         }
-        twoZeroFourEight.push(tempBoard);
+        if(!isIdentical(nowBoard, tempBoard)) {
+            twoZeroFourEight.push(tempBoard);
+        }
         
         // 좌측으로
-        memcpy(tempBoard.board, nowBoard.board, sizeof(gameBoard));
-        twoZeroFourEight.push(tempBoard);
+        memcpy(tempBoard.board, nowBoard.board, sizeof(nowBoard.board));
+        for(int i=0;i<n;++i) {
+            for(int j=0;j<n-1;++j) {
+                if(tempBoard.board[i][j] != 0 && tempBoard.board[i][j] == tempBoard.board[i][j+1]) {
+                    tempBoard.board[i][j] ++;
+                    tempBoard.board[i][j+1] = 0;
+                    findBlock[tempBoard.board[i][j]] = true;
+                }
+                if(tempBoard.board[i][j] == 0) {
+                    for(int k=j+1;k<=n-1;++k) {
+                        tempBoard.board[i][k - 1] = tempBoard.board[i][k];
+                    }
+                    tempBoard.board[i][n - 1] = 0;
+                }
+            }
+        }
+        if(!isIdentical(nowBoard, tempBoard)) {
+            twoZeroFourEight.push(tempBoard);
+        }
         
         // 위로
-        memcpy(tempBoard.board, nowBoard.board, sizeof(gameBoard));
-        twoZeroFourEight.push(tempBoard);
+        memcpy(tempBoard.board, nowBoard.board, sizeof(nowBoard.board));
+        for(int j=0;j<n;++j) {
+            for(int i=0;i<n-1;++i) {
+                if(tempBoard.board[i][j] != 0 && tempBoard.board[i][j] == tempBoard.board[i+1][j]) {
+                    tempBoard.board[i][j] ++;
+                    tempBoard.board[i+1][j] = 0;
+                    findBlock[tempBoard.board[i][j]] = true;
+                }
+                if(tempBoard.board[i][j] == 0) {
+                    for(int k=i+1;k<=n-1;++k) {
+                        tempBoard.board[k - 1][j] = tempBoard.board[k][j];
+                    }
+                    tempBoard.board[n - 1][j] = 0;
+                }  
+            }
+        }
+        if(!isIdentical(nowBoard, tempBoard)) {
+            twoZeroFourEight.push(tempBoard);
+        }
         
         // 아래로
-        memcpy(tempBoard.board, nowBoard.board, sizeof(gameBoard));
-        twoZeroFourEight.push(tempBoard);
+        memcpy(tempBoard.board, nowBoard.board, sizeof(nowBoard.board));
+        for(int j=0;j<n;++j) {
+            for(int i=n-1;i>=1;--i) {
+                if(tempBoard.board[i][j] != 0 && tempBoard.board[i][j] == tempBoard.board[i-1][j]) {
+                    tempBoard.board[i][j] ++;
+                    tempBoard.board[i-1][j] = 0;
+                    findBlock[tempBoard.board[i][j]] = true;
+                }
+                if(tempBoard.board[i][j] == 0) {
+                    for(int k=i-1;k>=0;--k) {
+                        tempBoard.board[k + 1][j] = tempBoard.board[k][j];
+                    }
+                    tempBoard.board[0][j] = 0;
+                }
+            }
+        }
+        if(!isIdentical(nowBoard, tempBoard)) {
+            twoZeroFourEight.push(tempBoard);
+        }
         
     }
 
@@ -68,12 +163,15 @@ int main(){
     for(int i=0;i<n;++i) {
         for(int j=0;j<n;++j) {
             cin >> temp;
-            temp = int(log2(double(temp)));
+            if(temp != 0){
+                temp = int(log2(double(temp)));
+            }
             initBoard.board[i][j] = temp;
             findBlock[temp] = true;
         }
     }
 
+    initBoard.convergeCount = 0;
     play2048();
     return 0;
 }
