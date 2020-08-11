66b66ea3a9eb8dcde79d17c954a1ad460850a711
pseudowasabi <y0s30b.k@gmail.com>
2020-08-11 22:25:26 +0900

    BOJ ID 2805 AC, 1년만에 다시 풀이 - parametric search 대표 문제

--- sds_algorithms/2/2143.cpp
+++ sds_algorithms/2/2143.cpp
@@ -1,1 +1,15 @@
-// 
+// 날리고 아예 다시 풀이함
+// 오늘 개념 공부하고 풀었던 문제들은, 9월 초중순에 다시 풀어볼 것
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    
+
+    return 0;
+}
