d60a7306103842e0bcf2aabea3984e0b35760964
pseudowasabi <y0s30b.k@gmail.com>
2020-08-15 21:01:50 +0900

    확장 유클리드를 구하는 두 가지 방법

--- sds_algorithms/4/gcd_example.cpp
+++ sds_algorithms/4/gcd_example.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <tuple>
 using namespace std;
 
 int gcd_recursion(int a, int b) {
@@ -20,7 +21,35 @@ int gcd(int a, int b) {
     // return abs(a);
 }
 
+tuple<int, int, int> extended_gcd(int a, int b) {
+    if(!b) {
+        return make_tuple(a, 1, 0);
+    }
+    int g, x, y;
+    tie(g, x, y) = extended_gcd(b, a % b);
+    cout << g << ' ' << y << ' ' << x-(a/b)*y << '\n';
+    return make_tuple(g, y, x-(a/b)*y);
+}
+
+tuple<int, int, int> extended_gcd2(int a, int b) {
+    int idx = 0;
+    int s[2] = {1, 0};
+    int t[2] = {0, 1};
+    int r[2] = {a, b};
+
+    while(r[idx^1]) {
+        cout << s[idx] << ' ' << t[idx] << ' ' << r[idx] << '\n';
+        s[idx] -= s[idx^1] * (r[idx] / r[idx^1]);
+        t[idx] -= t[idx^1] * (r[idx] / r[idx^1]);
+        r[idx] %= r[idx^1];
+        idx ^= 1;
+    }
+    return make_tuple(r[idx], s[idx], t[idx]);
+}
+
 int main(){
-    cout << gcd(36, 132);
+    int g, x, y;
+    tie(g, x, y) = extended_gcd(71, 240);
+    cout << g << ' ' << x << ' ' << y;
     return 0;
 }
